7793377cd09027ce767f20f2b5e9ea0b61c7ae43
Shivesh Kumar <97754871+shivesh179@users.noreply.github.com>
2022-10-02 13:37:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
 import './App.css';
+import Header from './components/header'
+import Sidebar from './components/sidebar'
+import FilesView from './components/filesView/FilesView'
+import SideIcons from './components/sideIcons'
+
+import GDriveLogo from './media/google-drive-logo.png'
+
+import { auth, provider } from "./firebase";
+import { useState } from 'react';
 
 function App() {
+  // const [user, setUser] = useState()
+  const [user, setUser] = useState({
+    displayName: "David Rakosi",
+    email: "david@cleverprogrammer.com",
+    emailVerified: true,
+    phoneNumber: null,
+    photoURL: "https://lh6.googleusercontent.com/-KyLTWqvDIHQ/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclcWGWqkt6YUAan32YO4CSR07Y2jw/s96-c/photo.jpg"
+  })
+
+  const handleLogin = () => {
+    if (!user) {
+      auth.signInWithPopup(provider).then((result) => {
+        setUser(result.user)
+        console.log(result.user)
+      }).catch((error) => {
+        alert(error.message);
+      });
+    } else if (user) {
+      auth.signOut().then(() => {
+        setUser(null)
+      }).catch((err) => alert(err.message))
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {
+        user ? (
+          <>
+            <Header userPhoto={user.photoURL} />
+            <div className="app__main">
+              <Sidebar />
+              <FilesView />
+              <SideIcons />
+            </div>
+          </>
+        ) : (
+            <div className='app__login'>
+              <img src={GDriveLogo} alt="Google Drive" />
+              <button onClick={handleLogin}>Log in to Google Drive</button>
+            </div>
+          )
+      }
     </div>
   );
 }
